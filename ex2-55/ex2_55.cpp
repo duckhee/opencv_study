93592b552b4d04a80814930b2406af20e01feb63
duckhee <fain9301@yahoo.com>
2018-11-07 14:36:59 +0900

    add ex2-56

--- ex2-55/ex2_55.cpp
+++ ex2-55/ex2_55.cpp
@@ -3,45 +3,71 @@
 using namespace cv;
 using namespace std;
 
+template <class T>
+T* vec_to_arr(vector<T> v1) //1D array
+{
+    T* v2 = new T[v1.size()];
+    for(int i = 0; i < v1.size(); i++)
+    {
+        v2[i] = v1[i];
+    }
+    return v2;
+}
+
+template<class T>
+void delete_arr(T* arr)
+{
+    delete[] arr;
+}
+
 int main()
 {
-    vector<Point3f> V1;
-    V1.push_back(Point3f(255, 0, 0));
-    V1.push_back(Point3f(0, 255, 0));
-    V1.push_back(Point3f(0, 0, 255));
-    V1.push_back(Point3f(0, 255, 255));
+    Mat dstImage(512, 512, CV_8UC3, Scalar(255, 255, 255));
 
-    Mat A(V1);
-    //Mat A(4, 1, CV_32FC3);
-    //memcpy(A.data, V1.data(), V1.size() * sizeof(Point3f)); //copy vector to mat
-    cout<<"A.channels() = "<<A.channels()<<endl;
-    cout<<"A.rows = "<<A.rows<<endl;
-    cout<<"A.cols = "<<A.cols<<endl;
-    cout<<"A = "<<A<<endl;
-    
-    vector<Point3f> V2;
-    A.copyTo(V2);
-    //V2.assign((Point3f *)A.datastart, (Point3f*)A.dataend);
+    vector<Point> contour;
+    contour.push_back(Point(100, 100));
+    contour.push_back(Point(200, 100));
+    contour.push_back(Point(200, 200));
+    contour.push_back(Point(100, 200));
 
-    cout<<"V2 = ";
-    for(int i = 0; i < V2.size(); i++)
+    int npts[] = {contour.size()};
+    int i;
+
+    Point *P1 = &contour[0];
+    for(i = 0; i < contour.size(); i++)
     {
-        cout<<(Point3f)V2[i]<<", ";
+        cout<<"P1["<<i<<"] = "<<P1[i]<<endl;
     }
-    cout<<endl;
+    //polylines(dstImage, (const Point**)&P1, npts, 1, true, Scalar(0, 0, 255));
 
-    Mat B = A.reshape(1);
-    Mat C = A.reshape(1).t(); //B.t()
+    Point *P2 = (Point *)Mat(contour).data;
+    for(i = 0; i < contour.size(); i++)
+    {
+        cout<<"P2["<<i<<"] = "<<P2[i]<<endl;
+    }
+    //polylines(dstImage, (const Point**)&P2, npts, 1, true, Scalar(0, 0, 255));
 
-    cout<<"B.channels() = "<<B.channels()<<endl;
-    cout<<"B.rows = "<<B.rows<<endl;
-    cout<<"B.cols = "<<B.cols<<endl;
-    cout<<"B = "<<B<<endl;
+    Point P3[4];
+    copy(contour.begin(), contour.end(), P3);
+    for(i = 0; i < contour.size(); i++)
+    {
+        cout<<"P3["<<i<<"] = "<<P3[i]<<endl;
+    }
 
-    cout<<"C.channels() = "<<C.channels()<<endl;
-    cout<<"C.rows = "<<C.rows<<endl;
-    cout<<"C.cols = "<<C.cols<<endl;
-    cout<<"C = "<<C<<endl;
+    //Point *ptrP3 = P3;
+    //polylines(dstImage, (const Point**)&P3, npts, 1, true, Scalar(0, 0, 255));
+
+    Point *P4 = vec_to_arr<Point>(contour);
+
+    for(i = 0; i < contour.size(); i++)
+    {
+        cout<<"P4["<<i<<"] = "<<P4[i]<<endl;
+    }
+    polylines(dstImage, (const Point**)&P4, npts, 1, true, Scalar(0, 0, 255));
+    delete_arr<Point>(P4);
+
+    imshow("dstImage", dstImage);
+    waitKey();
 
     return 0;
 }
