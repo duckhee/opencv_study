3559557444768c8c07ffa5a80c974d2526054e16
duckhee <fain9301@yahoo.com>
2018-11-08 13:34:37 +0900

    ex3-8

---
+++ chapter3/ex3-8/ex3_8.cpp
@@ -0,0 +1,28 @@
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    Mat dstImage(512, 512, CV_8UC3, Scalar(255, 255, 255));
+
+    string text = "OpenCV Programing";
+    int fontFace = FONT_HERSHEY_SIMPLEX;
+    double fontScale = 1.0;
+
+    int thickness = 1;
+    int baseLine;
+    Point org(100, 100);
+
+    putText(dstImage, text, org, fontFace, fontScale, Scalar(0, 0, 0));
+    Size size = getTextSize(text, fontFace, fontScale, thickness, &baseLine);
+    rectangle(dstImage, org, Point(org.x + size.width, org.y - size.height), Scalar(0, 0, 255));
+    circle(dstImage, org, 3, Scalar(255, 0, 0), 2);
+
+    imshow("dstImage", dstImage);
+    waitKey();
+
+    return 0;
+
+}
