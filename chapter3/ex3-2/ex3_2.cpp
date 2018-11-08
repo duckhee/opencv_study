f386398a6de490c33b8d0a1a203db17afa7dfa64
duckhee <fain9301@yahoo.com>
2018-11-08 10:22:37 +0900

    add ex3-2

--- chapter3/ex3-2/ex3_2.cpp
+++ chapter3/ex3-2/ex3_2.cpp
@@ -5,5 +5,29 @@ using namespace std;
 
 int main()
 {
+    Mat dstImage(512, 512, CV_8UC3, Scalar(255, 255, 255));
+
+    if(dstImage.empty())
+    {
+        return -1;
+    }
+
+    Rect imgRect(100, 100, 300, 300);
+    rectangle(dstImage, imgRect.tl(), imgRect.br(), Scalar(255, 0, 0), 2);
     
+    Point pt1(120, 50), pt2(300, 500);
+    line(dstImage, pt1, pt2, Scalar(0, 255, 0), 2);
+
+    clipLine(imgRect, pt1, pt2);
+    cout<<"pt1 = "<<pt1<<endl;
+    cout<<"pt2 = "<<pt2<<endl;
+
+    circle(dstImage, pt1, 5, Scalar(0, 0, 255), 2);
+    circle(dstImage, pt2, 5, Scalar(0, 0, 255), 2);
+    line(dstImage, pt1, pt2, Scalar(255, 0, 0), 2);
+
+    imshow("dstImage", dstImage);
+    waitKey();
+
+    return 0;
 }
