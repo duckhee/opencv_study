94c8965e9ad4994e7ad8c926f5fe2e73fef9b6a2
duckhee <fain9301@yahoo.com>
2018-11-16 17:24:13 +0900

    add ex5-20.cpp

---
+++ chapter5/ex5-20/ex5_20.cpp
@@ -0,0 +1,32 @@
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    uchar dataA[16] = {
+        0 ,0, 0, 0,
+        1, 1, 3, 5,
+        6, 1, 1, 3,
+        4, 3, 1, 7
+    };
+    Mat A(4, 4, CV_8U, dataA);
+    cout<<"A = "<<A<<endl;
+
+    int histSize[] = {8};
+    float valueRange[] = {0, 8};
+    const float* ranges[] = {valueRange};
+    int channels[] = {0};
+    int dims = 1;
+
+    Mat hist;
+    calcHist(&A, 1, channels, Mat(), hist, dims, histSize, ranges, true);
+    cout<<"hist="<<hist<<endl;
+
+    Mat backProject;
+    calcBackProject(&A, 1, channels, hist, backProject, ranges);
+    cout<<"backProject = "<<backProject<<endl;
+
+    return 0;
+}
